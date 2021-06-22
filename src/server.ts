3be395b70e244d26e482076b391544973b89c0cd
diogoscr <diogo.arauj@gmail.com>
2021-06-22 16:23:30 -0300

    Service and controller working

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import express, { response } from "express";
 import "reflect-metadata";
 
+import { router } from "./routes";
+
 import "./database";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(router);
 
 app.listen(3000, () => console.log("Server is running NLW"));
