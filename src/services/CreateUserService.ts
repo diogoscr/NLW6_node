b7066af999a1aae85d36d7655e7a3afa2349b28c
diogoscr <diogo.arauj@gmail.com>
2021-06-24 12:17:53 -0300

    User with encrypted pw

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from "typeorm";
 import { UsersRepositories } from "../repositories/UsersRepositories"
+import { hash } from "bcryptjs";
 
 interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean
+  password: string;
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const UsersRepository = getCustomRepository(UsersRepositories);
 
     console.log("Email", email);
@@ -26,10 +28,13 @@ class CreateUserService {
       throw new Error("User already exists");
     }
 
+    const passwordHash = await hash(password, 8)
+
     const user = UsersRepository.create({
       name,
       email,
-      admin
+      admin,
+      password: passwordHash,
     });
 
     await UsersRepository.save(user);
