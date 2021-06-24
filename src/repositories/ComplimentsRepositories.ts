00aa83555555e694741b7b0c56844371a826e87f
diogoscr <diogo.arauj@gmail.com>
2021-06-24 14:54:39 -0300

    Compliment service ok

---
+++ src/repositories/ComplimentsRepositories.ts
@@ -0,0 +1,8 @@
+import { Entity, Repository, EntityRepository } from 'typeorm';
+import { Compliment } from '../entities/Compliment';
+import { User } from '../entities/User';
+
+@EntityRepository(Compliment)
+class ComplimentsRepositories extends Repository<Compliment> { }
+
+export { ComplimentsRepositories };
